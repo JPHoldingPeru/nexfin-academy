c500ac4d0be4cb65ee4fd99ed12b5ff52352c32d
JP Holding <pastorb.jose@gmail.com>
2026-04-01 11:29:20 -0500

    Initialize Firebase with configuration settings

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,10 @@
+const firebaseConfig = {
+apiKey: "AQUI_TU_API_KEY",
+authDomain: "AQUI_TU_DOMINIO",
+projectId: "AQUI_TU_PROJECT_ID",
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
